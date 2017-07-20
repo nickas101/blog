a940b2a03b9d61189b4f56e57386a2f531d7fda7
Nick Astashkin <nick@7glyphs.com>
2017-07-21 00:33:24 +1200

    Home view is changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,32 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Recent posts</div>
-
+                <div class="panel-heading">Recent posts
+                    @if ($user->isAdmin)
+                        <span>
+                            <a style="text-align: right; float: right;" href="{{ action('PostsController@create') }}">
+                                    <button>Create new post</button>
+                                </a>
+                        </span>
+                    @endif
+                </div>
                 <div class="panel-body">
                     @foreach($posts as $post)
-                        <p>
-                            {{ $post->created_at }}
-                        </p>
-                        <p>
-                            {{ $post->text }}
-                        </p>
+                        <p><h2>{{ $post->title }}</h2> {{ $post->created_at }}</p>
+                        <p>{{ $post->text }}</p>
+                        @if ($user->isAdmin)
+                            <p>
+                                <a href="{{ action('PostsController@edit', ['id' => $post->id]) }}">
+                                    <button>Edit</button>
+                                </a>
+                                <a href="{{ action('PostsController@delete', ['id' => $post->id]) }}">
+                                    <button>Delete</button>
+                                </a>
+                            </p>
+                        @endif
                         <br>
                     @endforeach
                 </div>
-
             </div>
         </div>
     </div>
