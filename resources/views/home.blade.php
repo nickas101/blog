2fc99164ae6af054304730a1822563ed725cca81
Nick Astashkin <nick@7glyphs.com>
2017-07-20 17:03:16 +1200

    Posts are shown on the page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,20 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Recent posts</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    @foreach($posts as $post)
+                        <p>
+                            {{ $post->created_at }}
+                        </p>
+                        <p>
+                            {{ $post->text }}
+                        </p>
+                        <br>
+                    @endforeach
                 </div>
+
             </div>
         </div>
     </div>
